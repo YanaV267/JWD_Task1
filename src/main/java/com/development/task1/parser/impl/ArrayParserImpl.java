6ef541397d407804c6e1d0ff966ef5699627fc79
YanaV267 <yanochka.volkova.2002@gmail.com>
2021-10-12 15:50:12 +0300

    Initial commit

--- src/main/java/com/development/task1/parser/impl/ArrayParserImpl.java
+++ src/main/java/com/development/task1/parser/impl/ArrayParserImpl.java
@@ -5,14 +5,18 @@ import com.development.task1.entity.CustomArray;
 import com.development.task1.exception.CustomArrayException;
 import com.development.task1.parser.ArrayParser;
 import com.development.task1.validator.impl.ArrayValidatorImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
 
 public class ArrayParserImpl implements ArrayParser {
+    private static final Logger LOGGER = LogManager.getLogger(ArrayParserImpl.class);
 
     @Override
     public CustomArray parseNumber(String[] numberValues) throws CustomArrayException {
         if (numberValues == null) {
+            LOGGER.error("File is empty. No data was found in it.");
             throw new CustomArrayException("File is empty. No data was found in it.");
         }
         ArrayCreatorImpl arrayCreator = new ArrayCreatorImpl();
